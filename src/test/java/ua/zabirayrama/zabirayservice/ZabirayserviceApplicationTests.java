773ff56988165f41b26b545d187094e87cd71af2
mossberg500 <axaptasub@gmail.com>
2021-05-21 11:39:20 +0300

    first2105

---
+++ src/test/java/ua/zabirayrama/zabirayservice/ZabirayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.zabirayrama.zabirayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZabirayserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
